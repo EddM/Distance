1f87a98f0f3d9eec222bf675e22cbefbd1895936
EddM <eddm@me.com>
2013-12-15 20:48:09 +0000

    About page

---
+++ lib/game_states/about_state.rb
@@ -0,0 +1,21 @@
+class AboutState < GameState
+  include Interaction
+  
+  def initialize
+    @text = Gosu::Image.new($window, "res/about-text.png", false)
+    @back_button = Button.new(75, 650, :back, 108, 32, Proc.new {
+      $window.state_manager.pop
+    })
+  end
+
+  def update
+    @back_button.update
+  end
+
+  def draw
+    @text.draw 0, 0, Z::UI
+    @back_button.draw
+    draw_scope
+  end
+
+end
